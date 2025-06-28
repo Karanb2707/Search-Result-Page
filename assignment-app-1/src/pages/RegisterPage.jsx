2b91646436633fb233e5c52a6936389e0048d47f
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-06-28 15:37:17 +0530

    form style done

--- assignment-app-1/src/pages/RegisterPage.jsx
+++ assignment-app-1/src/pages/RegisterPage.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const RegisterPage = () => {
+
+    
+
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-300">
             <div className="flex bg-white shadow-lg shadow-slate-600 rounded-lg overflow-hidden w-full max-w-4xl border">
@@ -15,30 +18,46 @@ const RegisterPage = () => {
                     <h2 className="text-2xl font-bold mb-6 text-gray-800">Register</h2>
                     <form className="space-y-4">
                         <div>
-                            <label className="block text-sm font-medium text-gray-700">Name</label>
+                            <label htmlFor='name' className="block text-sm font-medium text-gray-700">Name</label>
                             <input
-                                type="text"
+                                type="name"
+                                autoComplete='off'
+                                name='name'
+                                id='name'
+                                placeholder='Name'
                                 className="mt-1 w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-1 focus:ring-red-700"
                             />
                         </div>
                         <div>
-                            <label className="block text-sm font-medium text-gray-700">Email</label>
+                            <label htmlFor='email' className="block text-sm font-medium text-gray-700">Email</label>
                             <input
                                 type="email"
+                                autoComplete='off'
+                                name='email'
+                                id='email'
+                                placeholder='Email'
                                 className="mt-1 w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-1 focus:ring-red-700"
                             />
                         </div>
                         <div>
-                            <label className="block text-sm font-medium text-gray-700">Password</label>
+                            <label htmlFor='password' className="block text-sm font-medium text-gray-700">Password</label>
                             <input
                                 type="password"
+                                autoComplete='off'
+                                name='password'
+                                id='password'
+                                placeholder='Password'
                                 className="mt-1 w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-1 focus:ring-red-700"
                             />
                         </div>
                         <div>
-                            <label className="block text-sm font-medium text-gray-700">Confirm Password</label>
+                            <label htmlFor='confirm_password' className="block text-sm font-medium text-gray-700">Confirm Password</label>
                             <input
                                 type="password"
+                                autoComplete='off'
+                                name='confirm_password'
+                                id='confirm_password'
+                                placeholder='Confirm Password'
                                 className="mt-1 w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-1 focus:ring-red-700"
                             />
                         </div>
